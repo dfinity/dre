f7e2df22a34f7aab60b6651e5b94b6c082af1c6f
Saša Tomić <sasa.tomic@dfinity.org>
2021-11-04 11:11:34 +0100

    Update the subnets to be updated in the next batch

--- bash-ops/update-multiple-subnets.sh
+++ bash-ops/update-multiple-subnets.sh
@@ -3,7 +3,7 @@
 GIT_REVISION="32d4e9c61c8b284d1bebed290df8d9b2efad2fc6"
 RC_BRANCH="rc--2021-10-24_18-31"
 
-for subnet in "4 (4zbus)" "9 (ejbmu)" "10 (eq6en)" "7 (5kdm2)"; do
+for subnet in "1 (snjp4)" "3 (pae4o)" "11 (csyj4)" "19 (jtdsg)"; do
     SUBNUM=$(echo "$subnet" | cut -d' ' -f1)
     export MOTIVATION="Upgrade the subnet $subnet to $RC_BRANCH"
     ./mainnet-op propose-to-update-subnet-replica-version "$SUBNUM" "$GIT_REVISION"
