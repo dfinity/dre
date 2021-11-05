f4003184a02567d844e8b2283970d445e904729b
Saša Tomić <sasa.tomic@dfinity.org>
2021-11-05 14:25:04 +0000

    Updated a list of subnets in update-multiple-subnets.sh

--- bash-ops/update-multiple-subnets.sh
+++ bash-ops/update-multiple-subnets.sh
@@ -6,10 +6,11 @@ GIT_REVISION="32d4e9c61c8b284d1bebed290df8d9b2efad2fc6"
 RC_BRANCH="rc--2021-10-24_18-31"
 
 SUBNETS=()
-SUBNETS+=("1 (snjp4)")
-SUBNETS+=("3 (pae4o)")
-SUBNETS+=("11 (csyj4)")
-SUBNETS+=("19 (jtdsg)")
+SUBNETS+=("12 (lspz2)")
+SUBNETS+=("17 (mpubz)")
+SUBNETS+=("18 (qdvhd)")
+SUBNETS+=("20 (k44fs)")
+SUBNETS+=("24 (4ecnw)")
 
 print_red() {
     echo -e "\033[0;31m$*\033[0m" 1>&2
