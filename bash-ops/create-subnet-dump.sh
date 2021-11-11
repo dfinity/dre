75c787fa88e8c149aa5176e2042ac6fcb80e8ccb
Saša Tomić <sasa.tomic@dfinity.org>
2021-11-11 17:42:12 +0100

    Rename bash-ops into scripts

--- bash-ops/create-subnet-dump.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -eEuo pipefail
-
-curl --fail --silent https://dashboard.mercury.dfinity.systems/api/proxy/registry/create_subnet\?size\=13  | jq -r  '.[] | .hostname' | tr '\n' ' ' | xargs ./decentralization-dump.sh none
