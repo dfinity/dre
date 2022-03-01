e9891237f185e82ab1b0d5e56c64b4c0eb0122d1
Saša Tomić <sasa.tomic@dfinity.org>
2022-03-01 12:34:43 +0100

    Fix qualification-new-release.sh paths for Python scripts

--- pylib/__fix_import_paths.py
+++ pylib/__fix_import_paths.py
@@ -1,5 +1,9 @@
 import subprocess
 import sys
 
-repo_root = subprocess.check_output(["git", "rev-parse", "--show-toplevel"]).decode("utf-8").rstrip()
+repo_root = (
+    subprocess.check_output(["git", "rev-parse", "--show-superproject-working-tree", "--show-toplevel"])
+    .decode("utf-8")
+    .splitlines()[0]
+)
 sys.path.append(repo_root)
