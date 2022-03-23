df780334d434f9214df58518849583f4968a4e10
Luka Skugor <luka.skugor@dfinity.org>
2022-03-23 15:35:41 +0100

    Update ic-admin to enable dry-run w/o HSM plugged in

--- src/defaults.rs
+++ src/defaults.rs
@@ -1,3 +1,3 @@
-pub(crate) const DEFAULT_IC_ADMIN_VERSION: &str = "936bf9ccaabd566c68232e5cb3f3ce7d5ae89328";
+pub(crate) const DEFAULT_IC_ADMIN_VERSION: &str = "a37500211e5a4578ac452490e16379cbb0c9282a";
 pub(crate) const DEFAULT_NNS_URL: &str = STAGING_NNS_URL;
 pub(crate) const STAGING_NNS_URL: &str = "http://[2600:3004:1200:1200:5000:7dff:fe29:a2f5]:8080";
