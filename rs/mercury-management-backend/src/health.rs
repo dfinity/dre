0cca034e53dcc963b9541ab63696aaae61ee4a06
Luka Skugor <luka.skugor@dfinity.org>
2022-06-16 18:37:34 +0200

    Show degraded nodes on the dashboard

--- rs/mercury-management-backend/src/health.rs
+++ rs/mercury-management-backend/src/health.rs
@@ -11,9 +11,10 @@ use strum_macros::EnumString;
 
 #[derive(EnumString, Serialize)]
 pub enum Status {
-    Unhealthy = 0,
-    Healthy = 1,
-    Unknown = 2,
+    Healthy,
+    Degraded,
+    Dead,
+    Unknown,
 }
 
 pub struct HealthClient {
@@ -45,7 +46,7 @@ impl HealthClient {
                 let status = if r.sample().value() == 1.0 {
                     Status::Healthy
                 } else {
-                    Status::Unhealthy
+                    Status::Dead
                 };
                 r.metric()
                     .get("ic_node")
@@ -58,18 +59,41 @@ impl HealthClient {
     pub async fn nodes(&self) -> anyhow::Result<HashMap<PrincipalId, Status>> {
         let query: InstantVector = InstantVector(format!(
             r#"
-                    bottomk by(ic_node) (1,
-                        (
-                            clamp_max(sum by (ic_node) (ALERTS{{alertstate="firing", ic="{network}", ic_node=~".+"}}), 0)
+                label_replace(
+                    ((
+                        sum by(ic_node) (
+                            up{{job="replica", ic="{network}"}}
                         )
-                            or
-                        (
-                            sum by(ic_node) (
-                                up{{job="orchestrator", ic="{network}"}}
-                            )
+                            or          
+                        sum by(ic_node) (
+                            up{{job="orchestrator", ic="{network}"}}
                         )
-                    )
-                "#,
+                    # swap 0 and 1 to get the health{{state="dead"}}=1 when up == 0
+                    ) - 1)^2 == 1
+                ,
+                    "state", "Dead", "", ""
+                )
+                    or ignoring(state)
+                label_replace(
+                    clamp_max(count by (ic_node) (ALERTS{{ic="{network}", severity="page"}}), 1)
+                ,
+                    "state", "Degraded", "", ""
+                )
+                    or ignoring(state)
+                label_replace(
+                    (
+                        sum by(ic_node) (
+                            up{{job="replica", ic="{network}"}}
+                        )
+                            or          
+                        sum by(ic_node) (
+                            up{{job="orchestrator", ic="{network}"}}
+                        )
+                    ) == 1
+                ,
+                    "state", "Healthy", "", ""
+                )
+            "#,
             network = self.network,
         ));
         let response = self.client.query(query, None, None).await?;
@@ -77,11 +101,8 @@ impl HealthClient {
         Ok(results
             .iter()
             .filter_map(|r| {
-                let status = if r.sample().value() == 1.0 {
-                    Status::Healthy
-                } else {
-                    Status::Unhealthy
-                };
+                let status = Status::from_str(r.metric().get("state").expect("all vectors should have a state label"))
+                    .expect("all vectors should have a valid label");
                 r.metric()
                     .get("ic_node")
                     .map(|id| (PrincipalId::from_str(id).unwrap(), status))
