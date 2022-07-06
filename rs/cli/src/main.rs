f5e4e9b72770012d26a69e60a2763abe321afb5c
Luka Skugor <luka.skugor@dfinity.org>
2022-07-06 10:09:40 +0000

    Auto-detect HSM & neuron, improve propose subcommand, dfx style --network flag

--- rs/cli/src/main.rs
+++ rs/cli/src/main.rs
@@ -1,31 +1,18 @@
 use clap::{CommandFactory, ErrorKind, Parser};
-use decentralization::SubnetChangeResponse;
-use dialoguer::Confirm;
-use dotenv::dotenv;
-use futures::Future;
-use ic_base_types::PrincipalId;
-use log::{error, info, warn};
-use mercury_management_types::{TopologyProposal, TopologyProposalKind, TopologyProposalStatus};
-use tokio::time::{sleep, Duration};
 mod cli;
 mod clients;
 pub(crate) mod defaults;
 mod ic_admin;
 mod ops_subnet_node_replace;
+mod runner;
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
-    init_env();
-
-    let cli_opts = cli::Opts::parse();
     init_logger();
+    let cli_opts = cli::Opts::parse();
+    let mut cmd = cli::Opts::command();
 
     ic_admin::with_ic_admin(Default::default(), async {
-        let runner = Runner {
-            ic_admin: ic_admin::Cli::from(&cli_opts),
-            dashboard_backend_client: clients::DashboardBackendClient::new(cli_opts.network, cli_opts.dev),
-        };
-
         // Start of actually doing stuff with commands.
         match &cli_opts.subcommand {
             cli::Commands::DerToPrincipal { path } => {
@@ -35,7 +22,7 @@ async fn main() -> Result<(), anyhow::Error> {
             }
 
             cli::Commands::Subnet(subnet) => {
-                let mut cmd = cli::Opts::command();
+                let runner = runner::Runner::from_opts(&cli_opts).await?;
                 match &subnet.subcommand {
                     cli::subnet::Commands::Deploy { .. } => {
                         if subnet.id.is_none() {
@@ -140,227 +127,18 @@ async fn main() -> Result<(), anyhow::Error> {
                 }
             }
 
-            cli::Commands::Get { args } => runner.ic_admin_generic_get(args),
+            cli::Commands::Get { args } => {
+                let ic_admin = ic_admin::Cli::from_opts(&cli_opts).await?;
+                ic_admin.run_passthrough_get(args)
+            },
 
-            cli::Commands::Propose { args } => runner.ic_admin_generic_propose(args),
+            cli::Commands::Propose { args } => {
+                let ic_admin = ic_admin::Cli::from_opts(&cli_opts).await?;
+                ic_admin.run_passthrough_propose(args)
+            },
         }
     })
     .await
-}
-
-#[derive(Clone)]
-pub struct Runner {
-    ic_admin: ic_admin::Cli,
-    dashboard_backend_client: clients::DashboardBackendClient,
-}
-
-impl Runner {
-    fn deploy(&self, subnet: &PrincipalId, version: &str) -> anyhow::Result<()> {
-        self.ic_admin
-            .propose_run(
-                ic_admin::ProposeCommand::UpdateSubnetReplicaVersion {
-                    subnet: *subnet,
-                    version: version.to_string(),
-                },
-                ic_admin::ProposeOptions {
-                    title: format!("Update subnet {subnet} to replica version {version}").into(),
-                    summary: format!("Update subnet {subnet} to replica version {version}").into(),
-                    motivation: None,
-                },
-            )
-            .map_err(|e| anyhow::anyhow!(e))?;
-
-        Ok(())
-    }
-
-    async fn membership_replace(
-        &self,
-        request: mercury_management_types::requests::MembershipReplaceRequest,
-    ) -> anyhow::Result<()> {
-        let change = self.dashboard_backend_client.membership_replace(request).await?;
-        self.swap_nodes(change).await
-    }
-
-    async fn swap_nodes(&self, change: SubnetChangeResponse) -> anyhow::Result<()> {
-        println!("{}", change);
-
-        self.with_confirmation(|r| {
-            let change = change.clone();
-            async move { r.run_swap_nodes(change).await }
-        })
-        .await
-    }
-
-    async fn run_swap_nodes(&self, change: SubnetChangeResponse) -> anyhow::Result<()> {
-        let subnet_id = change
-            .subnet_id
-            .ok_or_else(|| anyhow::anyhow!("subnet_id is required"))?;
-        let pending_action = self.dashboard_backend_client.subnet_pending_action(subnet_id).await?;
-        if let Some(proposal) = pending_action {
-            return Err(anyhow::anyhow!(vec![
-                format!(
-                    "There is a pending proposal for this subnet: https://dashboard.internetcomputer.org/proposal/{}",
-                    proposal.id
-                ),
-                format!(
-                    "Please finalize the last replacement first\n\n\t{} subnet --id {subnet_id} replace --finalize\n",
-                    std::env::args().next().unwrap_or_else(|| "release-cli".to_string())
-                )
-            ]
-            .join("\n")));
-        }
-
-        self.ic_admin
-            .propose_run(
-                ic_admin::ProposeCommand::AddNodesToSubnet {
-                    subnet_id,
-                    nodes: change.added.clone(),
-                },
-                ops_subnet_node_replace::replace_proposal_options(&change, None)?,
-            )
-            .map_err(|e| anyhow::anyhow!(e))?;
-
-        let add_proposal_id = if !self.ic_admin.dry_run {
-            self.wait_for_replacement_nodes(subnet_id).await?.0.id
-        } else {
-            const DUMMY_ID: u64 = 1234567890;
-            warn!("Set the first proposal ID to a dummy value: {}", DUMMY_ID);
-            DUMMY_ID
-        };
-        self.run_finalize_swap(change, add_proposal_id).await
-    }
-
-    async fn run_finalize_swap(&self, change: SubnetChangeResponse, add_proposal_id: u64) -> anyhow::Result<()> {
-        self.ic_admin
-            .propose_run(
-                ic_admin::ProposeCommand::RemoveNodesFromSubnet {
-                    nodes: change.removed.clone(),
-                },
-                ops_subnet_node_replace::replace_proposal_options(&change, add_proposal_id.into())?.with_motivation(format!("Finalize the replacements started with proposal [{add_proposal_id}](https://dashboard.internetcomputer.org/proposal/{add_proposal_id})")),
-            )
-            .map_err(|e| anyhow::anyhow!(e))?;
-
-        Ok(())
-    }
-
-    async fn run_cancel_swap(
-        &self,
-        change: SubnetChangeResponse,
-        add_proposal_id: u64,
-        motivation: String,
-    ) -> anyhow::Result<()> {
-        self.ic_admin
-            .propose_run(
-                ic_admin::ProposeCommand::RemoveNodesFromSubnet {
-                    nodes: change.added.clone(),
-                },
-                ops_subnet_node_replace::cancel_replace_proposal_options(&change, add_proposal_id)?
-                    .with_motivation(motivation),
-            )
-            .map_err(|e| anyhow::anyhow!(e))?;
-        Ok(())
-    }
-
-    async fn wait_for_replacement_nodes(
-        &self,
-        subnet_id: PrincipalId,
-    ) -> anyhow::Result<(TopologyProposal, SubnetChangeResponse)> {
-        Ok(loop {
-            if let Ok(Some(proposal)) = self.dashboard_backend_client.subnet_pending_action(subnet_id).await {
-                if let TopologyProposal {
-                    status: TopologyProposalStatus::Executed,
-                    kind: TopologyProposalKind::ReplaceNode(replace),
-                    ..
-                } = &proposal
-                {
-                    break (
-                        proposal.clone(),
-                        SubnetChangeResponse {
-                            added: replace.new_nodes.clone(),
-                            removed: replace.old_nodes.clone(),
-                            subnet_id: subnet_id.into(),
-                            ..Default::default()
-                        },
-                    );
-                }
-            }
-            sleep(Duration::from_secs(10)).await;
-        })
-    }
-
-    async fn recover_finalize_swap(&self, subnet_id: PrincipalId) -> anyhow::Result<()> {
-        let (topology_proposal, change) = self.wait_for_replacement_nodes(subnet_id).await?;
-
-        self.with_confirmation(|r| {
-            let change = change.clone();
-            let add_proposal_id = topology_proposal.id;
-            async move { r.run_finalize_swap(change, add_proposal_id).await }
-        })
-        .await
-    }
-
-    async fn cancel_swap(&self, subnet_id: PrincipalId, motivation: String) -> anyhow::Result<()> {
-        let (topology_proposal, change) = self.wait_for_replacement_nodes(subnet_id).await?;
-
-        self.with_confirmation(|r| {
-            let change = change.clone();
-            let add_proposal_id = topology_proposal.id;
-            let motivation = motivation.clone();
-            async move { r.run_cancel_swap(change, add_proposal_id, motivation).await }
-        })
-        .await
-    }
-
-    async fn with_confirmation<E, F>(&self, exec: E) -> anyhow::Result<()>
-    where
-        E: Fn(Self) -> F,
-        F: Future<Output = anyhow::Result<()>>,
-    {
-        if !self.ic_admin.dry_run {
-            exec(self.dry()).await?;
-            if !Confirm::new()
-                .with_prompt("Do you want to continue?")
-                .default(false)
-                .interact()?
-            {
-                return Err(anyhow::anyhow!("Action aborted"));
-            }
-        }
-
-        exec(self.clone()).await
-    }
-
-    fn ic_admin_generic_get(&self, args: &[String]) -> anyhow::Result<()> {
-        self.ic_admin.run_passthrough_get(args)
-    }
-
-    fn ic_admin_generic_propose(&self, args: &[String]) -> anyhow::Result<()> {
-        self.ic_admin.run_passthrough_propose(args)
-    }
-
-    fn dry(&self) -> Self {
-        Self {
-            ic_admin: self.ic_admin.dry_run(),
-            dashboard_backend_client: self.dashboard_backend_client.clone(),
-        }
-    }
-}
-
-fn init_env() {
-    match std::env::var("ENV") {
-        Ok(env_file) => {
-            if dotenv::from_filename(&env_file).is_err() {
-                error!("Supplied environment file {} not found", &env_file);
-                panic!("Failed to load the environment file")
-            }
-            info!("Initialized environment from {}", &env_file)
-        }
-        Err(_) => {
-            if dotenv().is_err() {
-                info!(".env file not found. You can copy env.template to .env to adjust configuration.");
-            }
-        }
-    }
 }
 
 fn init_logger() {
