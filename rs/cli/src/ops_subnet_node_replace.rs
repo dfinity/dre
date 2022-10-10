b54632073b187820331fc335ce2b5ebd6c17b2ee
Saša Tomić <sasa.tomic@dfinity.org>
2022-10-10 16:11:56 +0000

    Release cli: simplify summary for the node replacement proposal

--- rs/cli/src/ops_subnet_node_replace.rs
+++ rs/cli/src/ops_subnet_node_replace.rs
@@ -1,6 +1,5 @@
 use crate::ic_admin;
 use decentralization::SubnetChangeResponse;
-use ic_base_types::PrincipalId;
 
 #[cfg(test)]
 mod tests;
@@ -11,14 +10,6 @@ pub fn replace_proposal_options(change: &SubnetChangeResponse) -> anyhow::Result
         .ok_or_else(|| anyhow::anyhow!("subnet_id is required"))?
         .to_string();
 
-    let node_list_markdown = |principals: &[PrincipalId]| {
-        principals
-            .iter()
-            .map(|p| format!("- `{p}`"))
-            .collect::<Vec<_>>()
-            .join("\n")
-    };
-
     let replace_target = if change.added.len() > 1 || change.removed.len() > 1 {
         "nodes"
     } else {
@@ -28,19 +19,7 @@ pub fn replace_proposal_options(change: &SubnetChangeResponse) -> anyhow::Result
 
     Ok(ic_admin::ProposeOptions {
         title: format!("Replace {replace_target} in subnet {subnet_id_short}",).into(),
-        summary: format!(
-            r#"# Replace {replace_target} in subnet {subnet_id_short}.
-
-Add nodes:
-{add_nodes}
-
-Remove nodes:
-{remove_nodes}
-"#,
-            add_nodes = node_list_markdown(&change.added),
-            remove_nodes = node_list_markdown(&change.removed),
-        )
-        .into(),
+        summary: format!("# Replace {replace_target} in subnet {subnet_id_short}",).into(),
         motivation: change.motivation.clone(),
     })
 }
