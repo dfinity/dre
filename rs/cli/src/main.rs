a5e683e456d22509cb9d354c81d1efcbfa27c907
Manuel Amador Briz <manuel.amador@dfinity.org>
2023-06-14 13:26:12 +0000

    Move functionality of getting the governance canister version out to a...

--- rs/cli/src/main.rs
+++ rs/cli/src/main.rs
@@ -1,9 +1,8 @@
 use crate::cli::version::Commands::{Retire, Update};
 use clap::{error::ErrorKind, CommandFactory, Parser};
-use ic_agent::Agent;
+use ic_canisters::governance_canister_version;
 use ic_management_types::requests::NodesRemoveRequest;
 use ic_management_types::{MinNakamotoCoefficients, Network, NodeFeature};
-use ic_nns_constants::GOVERNANCE_CANISTER_ID;
 use std::collections::BTreeMap;
 use std::str::FromStr;
 
@@ -22,22 +21,8 @@ async fn main() -> Result<(), anyhow::Error> {
     let mut cli_opts = cli::Opts::parse();
     let mut cmd = cli::Opts::command();
 
-    let canister_agent = Agent::builder()
-        .with_transport(ic_agent::agent::http_transport::ReqwestHttpReplicaV2Transport::create(
-            cli_opts.network.get_url(),
-        )?)
-        .build()?;
-
-    let governance_canister_build = std::str::from_utf8(
-        &canister_agent
-            .read_state_canister_metadata(
-                candid::Principal::from_str(&GOVERNANCE_CANISTER_ID.to_string())
-                    .expect("failed to convert governance canister principal to candid type"),
-                "git_commit_id",
-            )
-            .await?,
-    )?
-    .to_string();
+    let governance_canister_v = governance_canister_version(cli_opts.network.get_url()).await?;
+    let governance_canister_build = governance_canister_v.stringified_hash;
 
     ic_admin::with_ic_admin(governance_canister_build.into(), async {
         // Start of actually doing stuff with commands.
