73109b3027166411e240acea9045943713083040
Saša Tomić <sasa.tomic@dfinity.org>
2023-07-26 13:15:19 +0000

    chore(backend): Separate and simplify the function for displaying proposed subnet changes

---
+++ rs/ic-management-backend/src/subnets.rs
@@ -0,0 +1,159 @@
+use std::collections::BTreeMap;
+
+use decentralization::{network::SubnetChange, SubnetChangeResponse};
+use ic_base_types::PrincipalId;
+use ic_management_types::{Node, TopologyChangeProposal};
+
+pub fn get_proposed_subnet_changes(
+    all_nodes: &BTreeMap<PrincipalId, Node>,
+    subnet: &ic_management_types::Subnet,
+) -> Result<SubnetChangeResponse, anyhow::Error> {
+    if let Some(proposal) = &subnet.proposal {
+        let proposal: &TopologyChangeProposal = proposal;
+        let change = SubnetChange {
+            id: subnet.principal,
+            old_nodes: subnet.nodes.iter().map(decentralization::network::Node::from).collect(),
+            new_nodes: subnet.nodes.iter().map(decentralization::network::Node::from).collect(),
+            min_nakamoto_coefficients: None,
+            comment: None,
+            run_log: vec![],
+        }
+        .with_nodes(
+            proposal
+                .node_ids_added
+                .iter()
+                .map(|p| decentralization::network::Node::from(all_nodes.get(p).unwrap()))
+                .collect::<Vec<_>>(),
+        )
+        .without_nodes(
+            proposal
+                .node_ids_removed
+                .iter()
+                .map(|p| decentralization::network::Node::from(all_nodes.get(p).unwrap()))
+                .collect::<Vec<_>>(),
+        );
+        let mut response = SubnetChangeResponse::from(&change);
+        response.proposal_id = Some(proposal.id);
+        Ok(response)
+    } else {
+        Err(anyhow::format_err!(
+            "subnet {} does not have open membership change proposals",
+            subnet.principal
+        ))
+    }
+}
+
+// Adding some tests to the above function
+#[cfg(test)]
+mod tests {
+    use std::net::Ipv6Addr;
+
+    use ic_management_types::{Datacenter, DatacenterOwner, Operator, Provider};
+
+    use super::*;
+
+    #[test]
+    fn test_subnet_changes_for_empty() {
+        // Create some test nodes
+        let subnet_id = PrincipalId::new_subnet_test_id(0);
+        let all_nodes = gen_test_nodes(subnet_id, 50, 0);
+        let subnet = ic_management_types::Subnet {
+            principal: subnet_id,
+            nodes: all_nodes.values().take(13).cloned().collect(),
+            ..Default::default()
+        };
+        let err = get_proposed_subnet_changes(&all_nodes, &subnet)
+            .unwrap_err()
+            .to_string();
+        assert_eq!(
+            err,
+            "subnet fscpm-uiaaa-aaaaa-aaaap-yai does not have open membership change proposals"
+        );
+    }
+
+    #[test]
+    fn test_subnet_changes_for_1_node() {
+        let subnet_id = PrincipalId::new_subnet_test_id(0);
+        let all_nodes = gen_test_nodes(subnet_id, 50, 0);
+        let subnet_size = 13;
+        let node_ids_added: Vec<PrincipalId> = all_nodes.keys().skip(subnet_size).take(1).cloned().collect();
+        let proposal_replace = TopologyChangeProposal {
+            node_ids_added: node_ids_added.clone(),
+            node_ids_removed: vec![],
+            subnet_id: Some(subnet_id),
+            id: 12345,
+        };
+        let subnet = ic_management_types::Subnet {
+            principal: subnet_id,
+            nodes: all_nodes.values().take(subnet_size).cloned().collect(),
+            proposal: Some(proposal_replace),
+            ..Default::default()
+        };
+        let change = get_proposed_subnet_changes(&all_nodes, &subnet).unwrap();
+        assert_eq!(change.added, node_ids_added);
+        assert_eq!(change.removed, vec![]);
+    }
+
+    #[test]
+    fn test_subnet_changes_for_multiple_add_remove_nodes() {
+        let subnet_id = PrincipalId::new_subnet_test_id(0);
+        let all_nodes = gen_test_nodes(subnet_id, 50, 0);
+        let subnet_size = 13;
+        let node_ids_added: Vec<PrincipalId> = all_nodes.keys().skip(subnet_size).take(3).cloned().collect();
+        let node_ids_removed: Vec<PrincipalId> = all_nodes.keys().take(2).cloned().collect();
+        let proposal_replace = TopologyChangeProposal {
+            node_ids_added: node_ids_added.clone(),
+            node_ids_removed: node_ids_removed.clone(),
+            subnet_id: Some(subnet_id),
+            id: 12345,
+        };
+        let subnet = ic_management_types::Subnet {
+            principal: subnet_id,
+            nodes: all_nodes.values().take(subnet_size).cloned().collect(),
+            proposal: Some(proposal_replace),
+            ..Default::default()
+        };
+        let change = get_proposed_subnet_changes(&all_nodes, &subnet).unwrap();
+        assert_eq!(change.added, node_ids_added);
+        assert_eq!(change.removed, node_ids_removed);
+    }
+
+    fn gen_test_nodes(subnet_id: PrincipalId, num_nodes: u64, start_at_number: u64) -> BTreeMap<PrincipalId, Node> {
+        let mut nodes = BTreeMap::new();
+        for i in start_at_number..start_at_number + num_nodes {
+            let node = Node {
+                principal: PrincipalId::new_node_test_id(i),
+                operator: Operator {
+                    principal: PrincipalId::new_user_test_id(i),
+                    provider: Provider {
+                        principal: PrincipalId::new_user_test_id(i),
+                        name: Some(format!("provider-{}", i)),
+                        ..Default::default()
+                    },
+                    datacenter: Some(Datacenter {
+                        name: format!("datacenter-{}", i),
+                        owner: DatacenterOwner {
+                            name: format!("datacenter-owner-{}", i),
+                        },
+                        city: format!("datacenter-city-{}", i),
+                        country: format!("datacenter-country-{}", i),
+                        continent: format!("datacenter-continent-{}", i),
+                        latitude: None,
+                        longitude: None,
+                    }),
+                    ..Default::default()
+                },
+                subnet_id: Some(subnet_id),
+                decentralized: true,
+                ip_addr: Ipv6Addr::new(0, 0, 0, 0, 0, 0, 0, 1),
+                hostname: None,
+                dfinity_owned: None,
+                proposal: None,
+                duplicates: None,
+                label: None,
+            };
+            nodes.insert(node.principal, node);
+        }
+        nodes
+    }
+}
