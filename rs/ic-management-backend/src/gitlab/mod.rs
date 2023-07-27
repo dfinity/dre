2cfa6a973eee86c286718ea2abff21a01e5e0608
Saša Tomić <sasa.tomic@dfinity.org>
2023-07-27 14:03:47 +0000

    chore(backend): Refactor backend and some other code, no functional change

--- rs/ic-management-backend/src/gitlab/mod.rs
+++ rs/ic-management-backend/src/gitlab/mod.rs
@@ -1,5 +1,6 @@
 mod commit_refs;
 pub use commit_refs::CommitRefs;
+use gitlab::{AsyncGitlab, GitlabBuilder};
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -7,4 +8,14 @@ pub struct CommitRef {
     #[serde(alias = "type")]
     pub kind: String,
     pub name: String,
+}
+
+pub async fn authenticated_client(env: &str) -> AsyncGitlab {
+    GitlabBuilder::new(
+        "gitlab.com",
+        std::env::var(env).unwrap_or_else(|_| panic!("missing {} env variable", env)),
+    )
+    .build_async()
+    .await
+    .expect("unable to initialize gitlab token")
 }
