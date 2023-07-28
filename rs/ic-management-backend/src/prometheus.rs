a41cd31b7eb6cafb8adb138a20e29435bf7aa5b4
Saša Tomić <sasa.tomic@dfinity.org>
2023-07-28 13:43:31 +0000

    Revert "Merge branch 'prom-to-vm' into 'main'"

--- rs/ic-management-backend/src/prometheus.rs
+++ rs/ic-management-backend/src/prometheus.rs
@@ -3,9 +3,7 @@ use prometheus_http_query::Client;
 
 pub fn client(network: &Network) -> Client {
     match network {
-        Network::Mainnet | Network::Staging => {
-            Client::try_from("https://vmselect.ch1-obs1.dfinity.network/select/0/prometheus").unwrap()
-        }
+        Network::Mainnet | Network::Staging => Client::try_from("https://prometheus.mainnet.dfinity.network").unwrap(),
         _ => Client::try_from("https://prometheus.testnet.dfinity.network").unwrap(),
     }
 }
