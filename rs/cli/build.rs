7a4afec1bc6b4e2228d5ca5fc51307077a9548ef
Luka Skugor <luka.skugor@dfinity.org>
2023-08-08 13:23:59 +0000

    Initial bazel setup

--- rs/cli/build.rs
+++ rs/cli/build.rs
@@ -3,7 +3,8 @@
 use std::process::Command;
 fn main() {
     // taken from https://stackoverflow.com/questions/43753491/include-git-commit-hash-as-string-into-rust-program
-    let output = Command::new("git").args(["rev-parse", "HEAD"]).output().unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
+    let git_hash = option_env!("GIT_HASH").map(String::from).unwrap_or_else(|| {
+        String::from_utf8(Command::new("git").args(["rev-parse", "HEAD"]).output().unwrap().stdout).unwrap()
+    });
     println!("cargo:rustc-env=GIT_HASH={}", git_hash);
 }
