c901111caf0108a7095863ddd62a669be00c617d
Luka Skugor <luka.skugor@dfinity.org>
2023-10-05 12:59:21 +0000

    initial bazelify python

--- pylib/__fix_import_paths.py
+++ pylib/__fix_import_paths.py
@@ -1,9 +1,11 @@
 import subprocess
 import sys
+import os
 
-repo_root = (
-    subprocess.check_output(["git", "rev-parse", "--show-superproject-working-tree", "--show-toplevel"])
-    .decode("utf-8")
-    .splitlines()[0]
-)
-sys.path.append(repo_root)
+if os.environ['BAZEL'] != 'true':
+    repo_root = (
+        subprocess.check_output(["git", "rev-parse", "--show-superproject-working-tree", "--show-toplevel"])
+        .decode("utf-8")
+        .splitlines()[0]
+    )
+    sys.path.append(repo_root)
