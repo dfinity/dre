ead3d1b7a7672e8787d653d1595be02016d0f4a3
Luka Skugor <luka.skugor@dfinity.org>
2023-10-13 18:35:30 +0200

    Update dashboard prometheus URLs

--- rs/ic-management-backend/src/prometheus.rs
+++ rs/ic-management-backend/src/prometheus.rs
@@ -3,7 +3,7 @@ use prometheus_http_query::Client;
 
 pub fn client(network: &Network) -> Client {
     match network {
-        Network::Mainnet | Network::Staging => Client::try_from("https://prometheus.mainnet.dfinity.network").unwrap(),
-        _ => Client::try_from("https://prometheus.testnet.dfinity.network").unwrap(),
+        Network::Mainnet => Client::try_from("https://victoria.mainnet.dfinity.network/select/0/prometheus/").unwrap(),
+        _ => Client::try_from("https://ic-metrics-victoria.ch1-obsstage1.dfinity.network/select/0/prometheus").unwrap(),
     }
 }
