583280b1730e17f7c41f267da35ec5d66948a9b9
Saša Tomić <sasa.tomic@dfinity.org>
2023-11-06 12:15:55 +0100

    chore(cli): Improve voting robustness

--- rs/cli/src/general.rs
+++ rs/cli/src/general.rs
@@ -1,4 +1,5 @@
-use std::time::Duration;
+use chrono::{DateTime, Utc};
+use std::{collections::HashSet, time::Duration};
 
 use ic_canisters::GovernanceCanisterWrapper;
 use ic_nns_governance::pb::v1::ProposalInfo;
@@ -20,26 +21,46 @@ pub(crate) async fn vote_on_proposals(
         Auth::Keyfile { path } => GovernanceCanisterWrapper::from_key_file(path.into(), nns_url)?,
     };
 
+    // In case of incorrectly set voting following, or in case of some other errors,
+    // we don't want to vote on the same proposal multiple times. So we keep an
+    // in-memory set of proposals that we already voted on.
+    let mut voted_proposals = HashSet::new();
+
     loop {
-        info!("Refreshing proposals...");
         let proposals = client.get_pending_proposals().await?;
         let proposals: Vec<&ProposalInfo> = proposals
             .iter()
-            .filter(|p| accepted_topics.contains(&p.topic) || !accepted_proposers.contains(&p.proposer.unwrap().id))
+            .filter(|p| accepted_topics.contains(&p.topic) && accepted_proposers.contains(&p.proposer.unwrap().id))
             .collect();
-        info!("Found total of {} proposals", proposals.len());
+        let proposals_to_vote = proposals
+            .iter()
+            .filter(|p| !voted_proposals.contains(&p.id.unwrap().id))
+            .collect::<Vec<_>>();
+        info!(
+            "Pending proposals {}, will vote on {} of them",
+            proposals.len(),
+            proposals_to_vote.len()
+        );
 
-        for proposal in proposals {
+        for proposal in proposals_to_vote.into_iter() {
             info!(
-                "Voting on proposal {} -> {}",
+                "Voting on proposal {} (topic {:?}, proposer {}) -> {}",
                 proposal.id.unwrap().id,
+                proposal.topic(),
+                proposal.proposer.unwrap_or_default().id,
                 proposal.proposal.clone().unwrap().title.unwrap()
             );
 
-            client.register_vote(neuron.id, proposal.id.unwrap().id).await?;
-            info!("Successfully voted on proposal {}", proposal.id.unwrap().id);
+            let response = client.register_vote(neuron.id, proposal.id.unwrap().id).await?;
+            info!("{}", response);
+            voted_proposals.insert(proposal.id.unwrap().id);
         }
 
+        let current_utc: DateTime<Utc> = Utc::now();
+        info!(
+            "{} UTC: sleeping 15s before another check for pending proposals...",
+            current_utc.format("%Y-%m-%d %H:%M:%S")
+        );
         let sleep = tokio::time::sleep(Duration::from_secs(15));
         tokio::select! {
             _ = tokio::signal::ctrl_c() => {
--- rs/ic-canisters/src/governance.rs
+++ rs/ic-canisters/src/governance.rs
@@ -7,7 +7,6 @@ use ic_nns_common::pb::v1::NeuronId;
 use ic_nns_common::pb::v1::ProposalId;
 use ic_nns_constants::GOVERNANCE_CANISTER_ID;
 use ic_nns_governance::pb::v1::manage_neuron::RegisterVote;
-use ic_nns_governance::pb::v1::manage_neuron_response::RegisterVoteResponse;
 use ic_nns_governance::pb::v1::ManageNeuron;
 use ic_nns_governance::pb::v1::ManageNeuronResponse;
 use ic_nns_governance::pb::v1::ProposalInfo;
@@ -81,23 +80,28 @@ impl GovernanceCanisterWrapper {
 
     pub async fn get_pending_proposals(&self) -> anyhow::Result<Vec<ProposalInfo>> {
         let vec: Vec<u8> = vec![];
-        match self
-            .agent
-            .execute_query(&GOVERNANCE_CANISTER_ID, "get_pending_proposals", Encode! { &vec }?)
-            .await
-        {
-            Ok(Some(response)) => match Decode!(response.as_slice(), Vec<ProposalInfo>) {
-                Ok(response) => Ok(response),
-                Err(err) => Err(anyhow::anyhow!("Error decoding response: {}", err)),
-            },
-            Ok(None) => Ok(vec![]),
-            Err(err) => Err(anyhow::anyhow!("Error executing query: {}", err)),
-        }
+        backoff::future::retry(backoff::ExponentialBackoff::default(), || async {
+            let empty_args = Encode! { &vec }.map_err(|err| backoff::Error::Permanent(anyhow::format_err!(err)))?;
+            match self
+                .agent
+                .execute_query(&GOVERNANCE_CANISTER_ID, "get_pending_proposals", empty_args)
+                .await
+            {
+                Ok(Some(response)) => match Decode!(response.as_slice(), Vec<ProposalInfo>) {
+                    Ok(response) => Ok(response),
+                    Err(err) => Err(anyhow::anyhow!("Error decoding response: {}", err)),
+                },
+                Ok(None) => Ok(vec![]),
+                Err(err) => Err(anyhow::anyhow!("Error executing query: {}", err)),
+            }
+            .map_err(|err| backoff::Error::Transient { err, retry_after: None })
+        })
+        .await
     }
 
-    pub async fn register_vote(&self, neuron_id: u64, proposal_id: u64) -> anyhow::Result<RegisterVoteResponse> {
-        let response = self
-            .manage_neuron(&ManageNeuron {
+    pub async fn register_vote(&self, neuron_id: u64, proposal_id: u64) -> anyhow::Result<String> {
+        let response = backoff::future::retry(backoff::ExponentialBackoff::default(), || async {
+            self.manage_neuron(&ManageNeuron {
                 id: Some(NeuronId { id: neuron_id }),
                 neuron_id_or_subaccount: None,
                 command: Some(ic_nns_governance::pb::v1::manage_neuron::Command::RegisterVote(
@@ -107,10 +111,29 @@ impl GovernanceCanisterWrapper {
                     },
                 )),
             })
-            .await?;
+            .await
+            .map_err(|err| backoff::Error::Transient { err, retry_after: None })
+        })
+        .await?;
+
         match response.command {
-            Some(ic_nns_governance::pb::v1::manage_neuron_response::Command::RegisterVote(response)) => Ok(response),
-            _ => Err(anyhow::anyhow!("Error registering vote")),
+            Some(ic_nns_governance::pb::v1::manage_neuron_response::Command::RegisterVote(response)) => {
+                Ok(format!("Successfully voted on proposal {} {:?}", proposal_id, response))
+            }
+            Some(ic_nns_governance::pb::v1::manage_neuron_response::Command::Error(err))
+                if err
+                    == ic_nns_governance::pb::v1::GovernanceError {
+                        error_type: ic_nns_governance::pb::v1::governance_error::ErrorTypeDesc::PreconditionFailed
+                            as i32,
+                        error_message: "Neuron already voted on proposal.".to_string(),
+                    } =>
+            {
+                Ok(format!(
+                    "Neuron already voted on proposal {}, cannot vote again.",
+                    proposal_id
+                ))
+            }
+            _err => Err(anyhow::anyhow!("Error registering vote: {:?}", _err)),
         }
     }
 
