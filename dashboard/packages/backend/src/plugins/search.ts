14352c789fa578c22470b7920ad7fd403cccbce8
Luka Skugor <luka.skugor@dfinity.org>
2023-11-16 17:39:27 +0000

    Fix backstage docker build

--- dashboard/packages/backend/src/plugins/search.ts
+++ dashboard/packages/backend/src/plugins/search.ts
@@ -5,8 +5,6 @@ import {
   LunrSearchEngine,
 } from '@backstage/plugin-search-backend-node';
 import { PluginEnvironment } from '../types';
-import { DefaultCatalogCollatorFactory } from '@backstage/plugin-catalog-backend';
-import { DefaultTechDocsCollatorFactory } from '@backstage/plugin-techdocs-backend';
 import { Router } from 'express';
 
 export default async function createPlugin(
@@ -19,34 +17,6 @@ export default async function createPlugin(
   const indexBuilder = new IndexBuilder({
     logger: env.logger,
     searchEngine,
-  });
-
-  const schedule = env.scheduler.createScheduledTaskRunner({
-    frequency: { minutes: 10 },
-    timeout: { minutes: 15 },
-    // A 3 second delay gives the backend server a chance to initialize before
-    // any collators are executed, which may attempt requests against the API.
-    initialDelay: { seconds: 3 },
-  });
-
-  // Collators are responsible for gathering documents known to plugins. This
-  // collator gathers entities from the software catalog.
-  indexBuilder.addCollator({
-    schedule,
-    factory: DefaultCatalogCollatorFactory.fromConfig(env.config, {
-      discovery: env.discovery,
-      tokenManager: env.tokenManager,
-    }),
-  });
-
-  // collator gathers entities from techdocs.
-  indexBuilder.addCollator({
-    schedule,
-    factory: DefaultTechDocsCollatorFactory.fromConfig(env.config, {
-      discovery: env.discovery,
-      logger: env.logger,
-      tokenManager: env.tokenManager,
-    }),
   });
 
   // The scheduler controls when documents are gathered from collators and sent
