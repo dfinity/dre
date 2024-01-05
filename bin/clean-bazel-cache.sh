b11dd2418ebf1e42c1b27cd6a6f473e9b83da5bf
Saša Tomić <sasa.tomic@dfinity.org>
2024-01-05 16:02:05 +0100

    Go back to a separate script

---
+++ bin/clean-bazel-cache.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+#
+# Clean up bazel cache if older than 7 days
+#
+
+set -eEuo pipefail
+
+echo "Bazel cache directory contents:"
+ls -l --full-time ~/.cache/bazel
+find ~/.cache/bazel -mindepth 1 -maxdepth 1 -type d -mtime +7 -exec sudo rm -rvf '{}' +
