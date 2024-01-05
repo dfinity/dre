83d421aa1c2f0249e6b9b65002a4b7937a2562cc
Saša Tomić <sasa.tomic@dfinity.org>
2024-01-05 18:05:40 +0100

    Handle non-existing bazel cache

--- bin/clean-bazel-cache.sh
+++ bin/clean-bazel-cache.sh
@@ -6,5 +6,5 @@
 set -eEuo pipefail
 
 echo "Bazel cache directory contents:"
-ls -l --full-time ~/.cache/bazel
+ls -l --full-time ~/.cache/bazell || exit 0  # In case bazel cache does not exist on github, it won't be mounted so we need to bail out here
 find ~/.cache/bazel -mindepth 1 -maxdepth 1 -type d -mtime +7 -exec sudo rm -rvf '{}' +
