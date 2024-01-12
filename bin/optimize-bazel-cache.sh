19c7299f425cc4c0db31fa4a9950d296ef9db79b
Saša Tomić <sasa.tomic@dfinity.org>
2024-01-12 18:40:14 +0100

    perf(ci): Reduce the bazel cache size (#56)
    * perf(ci): Reduce the bazel cache size
    
    * Less aggressive freeing up of disk space
    
    * Update comment

---
+++ bin/optimize-bazel-cache.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# Optimize the size of bazel cache
+#
+
+set -eEuo pipefail
+
+echo "Bazel cache directory contents:"
+ls -l --full-time ~/.cache/bazel || exit 0  # In case bazel cache isn't mounted yet on github, we need to stop here
+du -sh --total ~/.cache/bazel/*
+sudo apt install -qy rdfind
+sudo rdfind -makehardlinks true "$(bazel info output_base | grep .cache | tail -n1)"
