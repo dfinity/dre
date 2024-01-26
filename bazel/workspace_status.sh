9afce1e7d5f0dfe5fb3a3d9ddc76d5663736b970
Saša Tomić <sasa.tomic@dfinity.org>
2024-01-26 17:49:55 +0000

    simplify

--- bazel/workspace_status.sh
+++ bazel/workspace_status.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 echo "BUILD_TIME \"$(TZ=UTC date --rfc-3339=seconds)\""
 
 GIT_REV=$(git tag --points-at HEAD)
-if [[ -z "${GITREV:-}" ]]; then GIT_REV=$(git rev-parse HEAD); fi
+if [[ -z "${GITREV:-}" ]]; then GIT_REV=$(git describe --always --dirty); fi
 
 echo "GIT_REV ${GIT_REV:-unset}"
 
--- rs/cli/build.rs
+++ rs/cli/build.rs
@@ -1,10 +1,18 @@
-// build.rs
-
+// Construct a useful and specific version string for the CLI
 use std::process::Command;
+
 fn main() {
     // taken from https://stackoverflow.com/questions/43753491/include-git-commit-hash-as-string-into-rust-program
     let git_rev = option_env!("GIT_REV").map(String::from).unwrap_or_else(|| {
-        String::from_utf8(Command::new("git").args(["rev-parse", "HEAD"]).output().unwrap().stdout).unwrap()
+        String::from_utf8(
+            // https://stackoverflow.com/questions/21017300/git-command-to-get-head-sha1-with-dirty-suffix-if-workspace-is-not-clean
+            Command::new("git")
+                .args(["describe", "--always", "--dirty"])
+                .output()
+                .unwrap()
+                .stdout,
+        )
+        .unwrap()
     });
     if !git_rev.is_empty() {
         println!(
