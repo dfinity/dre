bb9fc41b3ebb9b7adf4fc7b2c9dd6f417c7ff9a0
pietrodimarco-dfinity <124565147+pietrodimarco-dfinity@users.noreply.github.com>
2024-01-29 23:30:24 +0100

    Fix test in service-discovery to be applied in mainnet registry (#111)
    * Add mainnet test using Cargo
    * Add mainnet test using Bazel
    * Add utility function to download and decompress archives

---
+++ rs/archive-utils/src/lib.rs
@@ -0,0 +1,76 @@
+use std::{io::{self, Read}, path::Path};
+use std::sync::{Arc, Mutex};
+use futures_lite::StreamExt;
+use flate2::read::GzDecoder;
+use tar::Archive;
+
+pub async fn download_and_extract(url: &str, output_target_dir: &Path) -> io::Result<()> {
+    let client = reqwest::Client::new();
+    let output_dir = Arc::new(Mutex::new(output_target_dir.to_path_buf()));
+    let response;
+
+    match client.get(url).send().await {
+        Ok(res) => response = res,
+        Err(error) => {
+            return Err(io::Error::new(io::ErrorKind::InvalidData, error));
+        }
+    };
+    let (tx, rx) = flume::bounded(0);
+
+    let decoder_thread = std::thread::spawn(move || {
+        let input = ChannelRead::new(rx);
+        let gz = GzDecoder::new(input);
+        let mut archive = Archive::new(gz);
+        archive.unpack(output_dir.lock().unwrap().clone()).unwrap();
+    });
+
+    if response.status() == reqwest::StatusCode::OK {
+        let mut stream = response.bytes_stream();
+
+
+        while let Some(item) = stream.next().await {
+            let chunk = item
+                .or(Err(format!("Error while downloading file")))
+                .unwrap();
+            tx.send_async(chunk.to_vec()).await.unwrap();
+        }
+        drop(tx);
+    }
+
+    tokio::task::spawn_blocking(|| decoder_thread.join())
+        .await
+        .unwrap()
+        .unwrap();
+
+    Ok(())
+}
+
+// Wrap a channel into something that impls `io::Read`
+struct ChannelRead {
+    rx: flume::Receiver<Vec<u8>>,
+    current: io::Cursor<Vec<u8>>,
+}
+
+impl ChannelRead {
+    fn new(rx: flume::Receiver<Vec<u8>>) -> ChannelRead {
+        ChannelRead {
+            rx,
+            current: io::Cursor::new(vec![]),
+        }
+    }
+}
+
+impl Read for ChannelRead {
+    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
+        if self.current.position() == self.current.get_ref().len() as u64 {
+            // We've exhausted the previous chunk, get a new one.
+            if let Ok(vec) = self.rx.recv() {
+                self.current = io::Cursor::new(vec);
+            }
+            // If recv() "fails", it means the sender closed its part of
+            // the channel, which means EOF. Propagate EOF by allowing
+            // a read from the exhausted cursor.
+        }
+        self.current.read(buf)
+    }
+}
