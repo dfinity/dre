83088805532e9b2b77c481ea6c7b697325b1b4ca
Pietro <pietro.di.marco@dfinity.org>
2024-02-02 19:12:55 +0100

    Introduced change in MSD to test diff

--- rs/ic-observability/multiservice-discovery-shared/src/builders/prometheus_config_structure.rs
+++ rs/ic-observability/multiservice-discovery-shared/src/builders/prometheus_config_structure.rs
@@ -29,7 +29,7 @@ impl Serialize for PrometheusFileSdConfig {
 #[derive(Debug, Clone)]
 pub struct PrometheusConfigBuilder {}
 
-pub const IC_NAME: &str = "ic";
+pub const IC_NAME: &str = "ic-diff";
 pub const IC_NODE: &str = "ic_node";
 pub const IC_SUBNET: &str = "ic_subnet";
 pub const JOB: &str = "job";
