a45bac6d6385d831d898aac3c438d12da2f699cc
Pietro <pietro.di.marco@dfinity.org>
2024-02-18 17:52:08 +0100

    Add tests

--- rs/ic-observability/multiservice-discovery/src/server_handlers/filters.rs
+++ rs/ic-observability/multiservice-discovery/src/server_handlers/filters.rs
@@ -12,6 +12,18 @@ pub(crate) struct AttributesFilter {
     node_provider_id: Option<PrincipalId>
 }
 
+impl Default for AttributesFilter {
+    fn default() -> Self {
+        Self { 
+            ic_name: Default::default(), 
+            dc_id: Default::default(), 
+            operator_id: Default::default(), 
+            subnet_id: Default::default(), 
+            node_provider_id: Default::default() 
+        }
+    }
+}
+
 impl AttributesFilter {
     fn filter_step<T: PartialEq>(maybe_want: &Option<T>, got: &T) -> bool {
         maybe_want.as_ref().map_or(true, |want| want == got)
@@ -27,5 +39,54 @@ impl AttributesFilter {
             (Some(_), _) => false,
             _ => true
         }
+    }
+}
+
+mod tests {
+    use std::{collections::BTreeSet, net::SocketAddrV6, str::FromStr};
+    use ic_types::{NodeId, PrincipalId, SubnetId};
+    use service_discovery::TargetGroup;
+
+    use crate::server_handlers::filters::AttributesFilter;
+
+    #[test]
+    fn attributes_filter_test() {
+        let target_group = TargetGroup {
+            node_id: NodeId::from(PrincipalId::new_anonymous()),
+            ic_name: "mercury".into(),
+            targets: BTreeSet::new(),
+            subnet_id: Some(SubnetId::from(
+                PrincipalId::from_str("x33ed-h457x-bsgyx-oqxqf-6pzwv-wkhzr-rm2j3-npodi-purzm-n66cg-gae").unwrap()
+            )),
+            dc_id: "test".into(),
+            operator_id: PrincipalId::new_anonymous(),
+            node_provider_id: PrincipalId::new_anonymous(),
+        };
+
+        let filter = AttributesFilter{
+            ic_name: Some("mercury".into()),
+            ..Default::default()
+        };
+        assert!(filter.filter(&target_group));
+
+        let filter = AttributesFilter{
+            ic_name: Some("other_ic".into()),
+            ..Default::default()
+        };
+        assert!(!filter.filter(&target_group));
+
+        let filter = AttributesFilter{
+            subnet_id: Some(SubnetId::from(
+                PrincipalId::from_str("x33ed-h457x-bsgyx-oqxqf-6pzwv-wkhzr-rm2j3-npodi-purzm-n66cg-gae").unwrap()
+            )),
+            ..Default::default()
+        };
+        assert!(filter.filter(&target_group));
+
+        let filter = AttributesFilter{
+            subnet_id: Some(SubnetId::from(PrincipalId::new_anonymous())),
+            ..Default::default()
+        };
+        assert!(!filter.filter(&target_group));
     }
 }
