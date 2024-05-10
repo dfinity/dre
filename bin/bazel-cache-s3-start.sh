0405cda711512f32e5f238d1d27ec8dce3adb9cf
Saša Tomić <sasa.tomic@dfinity.org>
2024-05-10 16:10:53 +0200

    feat(ci): Bazel remote cache on s3

---
+++ bin/bazel-cache-s3-start.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eExuo pipefail
+
+docker run --detach --name=bazel-remote -u $(id -u):$(id -g) \
+    -v ~/.cache/bazel-remote:/data -v $HOME/.aws:/aws-config \
+    -p 9090:8080 -p 9092:9092 \
+    quay.io/bazel-remote/bazel-remote \
+    --max_size 20 \
+    --s3.auth_method=aws_credentials_file --s3.aws_profile=wasabi --s3.aws_shared_credentials_file=/aws-config/credentials --s3.bucket=dre-ci-cache --s3.endpoint=s3.eu-central-2.wasabisys.com
