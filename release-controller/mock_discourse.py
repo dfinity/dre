846345acb80e711f6f8de41f0bfecec7bfd28285
Saša Tomić <sasa.tomic@dfinity.org>
2024-05-17 17:52:52 +0200

    fix(release controller): Only update posts if different and can_edit (#391)
    * fix(release controller): Only update posts if different and can_edit
    
    Also update some related tests
    
    * Some missing files
    
    * some missing files and reviewer feedback applied
    
    * Add back attributes required in tests

--- release-controller/mock_discourse.py
+++ release-controller/mock_discourse.py
@@ -2,12 +2,19 @@ from pydiscourse import DiscourseClient
 
 
 class DiscourseClientMock(DiscourseClient):
+    """A mock Discourse client."""
+
     def __init__(self):
+        """Create a new mock client. The actual host needs to be mocked in the test."""
+        self.host = "http://localhost:55555/"
         self.created_topics = []
         self.created_posts = []
         self.api_username = "test"
+        self.api_key = "test_api_key"
+        self.timeout = 10
 
     def categories(self):
+        """Return a list of categories."""
         return [
             {"id": i} | t
             for i, t in enumerate(
@@ -21,9 +28,11 @@ class DiscourseClientMock(DiscourseClient):
         ]
 
     def topics_by(self, _: str):
+        """Return a list of topics."""
         return [{"id": i + 1} | t for i, t in enumerate(self.created_topics)]
 
     def topic_posts(self, topic_id: str):
+        """Return a list of posts in a topic."""
         return {
             "post_stream": {
                 "posts": [
@@ -40,20 +49,23 @@ class DiscourseClientMock(DiscourseClient):
         category_id=None,
         topic_id=None,
         title=None,
-        tags=[],
-        **kwargs,
+        tags=None,
+        **kwargs,  # pylint: disable=unused-argument
     ):
+        """Create a new post. If topic_id is not provided, a new topic is created."""
         if not topic_id:
-            self.created_topics.append({"title": title, "category_id": category_id, "tags": tags})
+            self.created_topics.append({"title": title, "category_id": category_id, "tags": tags or []})
             topic_id = self.topics_by("")[-1]["id"]
         self.created_posts.append(
             {
                 "raw": content,
                 "topic_id": topic_id,
                 "yours": True,
+                "can_edit": True,
             }
         )
         return self.topic_posts(topic_id=topic_id)["post_stream"]["posts"][-1]
 
-    def update_post(self, post_id, content, edit_reason="", **kwargs):
+    def update_post(self, post_id, content, edit_reason="", **kwargs):  # pylint: disable=unused-argument
+        """Update an existing post."""
         self.created_posts[post_id - 1]["raw"] = content
