882365b2cf4179dea38c8675b26bd71400de8fad
Saša Tomić <sasa.tomic@dfinity.org>
2024-07-02 13:00:06 +0000

    Print also DC/NP info for unhealthy upgraded nodes

--- rs/cli/src/operations/hostos_rollout.rs
+++ rs/cli/src/operations/hostos_rollout.rs
@@ -534,9 +534,11 @@ impl HostosRollout {
                 unhealthy_nodes_on_the_new_version
                     .iter()
                     .map(|node| format!(
-                        "{}: {:?}",
+                        "{}: {:?} DC {} Node Provider {}",
                         node.principal,
-                        nodes_health.get(&node.principal).cloned().unwrap_or(Status::Unknown)
+                        nodes_health.get(&node.principal).cloned().unwrap_or(Status::Unknown),
+                        node.operator.datacenter.as_ref().map(|dc| dc.name.as_str()).unwrap_or("-"),
+                        node.operator.provider.name.as_deref().unwrap_or("-"),
                     ))
                     .collect::<Vec<_>>()
                     .join("\n")
