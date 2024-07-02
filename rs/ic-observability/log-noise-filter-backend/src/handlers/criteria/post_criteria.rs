68b109e4fb00857b5dd8cfbba6e44d02aa0a699c
Nikola Milosavljevic <73236646+NikolaMilosa@users.noreply.github.com>
2024-07-02 14:34:16 +0200

    feat(logs): Log noise filter (#348)

---
+++ rs/ic-observability/log-noise-filter-backend/src/handlers/criteria/post_criteria.rs
@@ -0,0 +1,12 @@
+use std::collections::BTreeMap;
+
+use axum::{extract::State, http::StatusCode, Json};
+
+use crate::handlers::Server;
+
+pub async fn update(State(state): State<Server>, Json(criteria): Json<Vec<String>>) -> Result<Json<BTreeMap<u32, String>>, (StatusCode, String)> {
+    match state.update_criteria(criteria).await {
+        Ok(()) => Ok(Json(state.get_criteria_mapped().await)),
+        Err(v) => Err((StatusCode::BAD_REQUEST, format!("Invalid instances of regex: {:?}", v))),
+    }
+}
