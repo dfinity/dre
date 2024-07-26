106e8a43e90efe4639c7d776199c6f95c12ade79
Luka Skugor <luka.skugor@chimeramail.com>
2024-07-26 10:40:52 +0200

    Update pytest.py

--- release-controller/pytest.py
+++ release-controller/pytest.py
@@ -5,4 +5,4 @@ if __name__ == "__main__":
     import pytest
 
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    raise SystemExit(pytest.main([dir_path]))
+    raise SystemExit(pytest.main([dir_path, "-vv"]))
