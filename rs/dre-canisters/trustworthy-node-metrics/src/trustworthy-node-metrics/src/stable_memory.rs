f3974fc8702c354bf98d5ab0116b422afb7ca632
pietrodimarco-dfinity <124565147+pietrodimarco-dfinity@users.noreply.github.com>
2024-08-13 16:46:06 +0200

    Improving Node-Metrics backend canister (#712)

--- rs/dre-canisters/trustworthy-node-metrics/src/trustworthy-node-metrics/src/stable_memory.rs
+++ rs/dre-canisters/trustworthy-node-metrics/src/trustworthy-node-metrics/src/stable_memory.rs
@@ -1,11 +1,17 @@
 use candid::{Decode, Encode, Principal};
+use ic_stable_structures::memory_manager::{MemoryId, MemoryManager, VirtualMemory};
 use ic_stable_structures::{storable::Bound, Storable};
 use ic_stable_structures::{DefaultMemoryImpl, StableBTreeMap};
 use itertools::Itertools;
 use std::borrow::Cow;
 use std::cell::RefCell;
+use std::collections::BTreeMap;
 
 use crate::types::{NodeMetricsStored, NodeMetricsStoredKey, TimestampNanos};
+
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+const MAX_VALUE_SIZE_BYTE: u32 = 102;
 
 impl Storable for NodeMetricsStored {
     fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
@@ -16,42 +22,49 @@ impl Storable for NodeMetricsStored {
         Decode!(bytes.as_ref(), Self).unwrap()
     }
 
-    const BOUND: Bound = Bound::Unbounded;
+    const BOUND: Bound = Bound::Bounded {
+        max_size: MAX_VALUE_SIZE_BYTE,
+        is_fixed_size: false,
+    };
 }
 
 thread_local! {
-    pub static MAP: RefCell<StableBTreeMap<NodeMetricsStoredKey, NodeMetricsStored, DefaultMemoryImpl>> =
-      RefCell::new(StableBTreeMap::init(DefaultMemoryImpl::default()));
+
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+    RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static NODE_METRICS_MAP: RefCell<StableBTreeMap<NodeMetricsStoredKey, NodeMetricsStored, Memory>> =
+      RefCell::new(StableBTreeMap::init(
+        MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0)))
+    ));
+
 }
 
 pub fn insert(key: NodeMetricsStoredKey, value: NodeMetricsStored) {
-    MAP.with(|p| p.borrow_mut().insert(key, value));
+    NODE_METRICS_MAP.with(|p| p.borrow_mut().insert(key, value));
 }
 
 pub fn latest_ts() -> Option<TimestampNanos> {
-    MAP.with(|p| p.borrow().last_key_value()).map(|((ts, _), _)| ts)
-}
-
-#[allow(dead_code)]
-pub fn get(node_metrics_key: &NodeMetricsStoredKey) -> Option<NodeMetricsStored> {
-    MAP.with(|p| p.borrow().get(node_metrics_key))
+    NODE_METRICS_MAP.with(|p| p.borrow().last_key_value()).map(|((ts, _), _)| ts)
 }
 
 pub fn get_metrics_range(from_ts: TimestampNanos, to_ts: Option<TimestampNanos>) -> Vec<(NodeMetricsStoredKey, NodeMetricsStored)> {
-    let range = {
-        let first = (from_ts, Principal::anonymous());
-        let last = (to_ts.unwrap_or(u64::MAX), Principal::anonymous());
-        first..=last
-    };
+    NODE_METRICS_MAP.with(|p| {
+        let to_ts = to_ts.unwrap_or(u64::MAX);
 
-    MAP.with(|p| p.borrow().range(range).collect_vec())
+        p.borrow().iter().filter(|((ts, _), _)| *ts >= from_ts && *ts <= to_ts).collect_vec()
+    })
 }
 
-pub fn metrics_before_ts(principal: Principal, ts: u64) -> Option<(NodeMetricsStoredKey, NodeMetricsStored)> {
-    MAP.with(|p| {
-        p.borrow()
-            .range((u64::MIN, principal)..(ts, principal))
-            .filter(|((_, p), _)| p == &principal)
-            .last()
-    })
+pub fn latest_metrics(principals: Vec<Principal>) -> BTreeMap<Principal, NodeMetricsStored> {
+    let mut latest_metrics = BTreeMap::new();
+    NODE_METRICS_MAP.with(|p| {
+        for ((_, principal), value) in p.borrow().iter() {
+            if principals.contains(&principal) {
+                latest_metrics.insert(principal, value);
+            }
+        }
+    });
+
+    latest_metrics
 }
