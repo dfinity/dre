1fb7f604dc02c8acaa4a568af077105b9cc67c53
pietrodimarco-dfinity <124565147+pietrodimarco-dfinity@users.noreply.github.com>
2024-08-14 10:39:29 +0200

    Better naming rewards reduction computation (#722)

--- rs/dre-canisters/trustworthy-node-metrics/src/trustworthy-node-metrics/src/rewards_manager.rs
+++ rs/dre-canisters/trustworthy-node-metrics/src/trustworthy-node-metrics/src/rewards_manager.rs
@@ -5,6 +5,28 @@ use crate::types::{DailyNodeMetrics, TimestampNanos};
 const MIN_FAILURE_RATE: f64 = 0.1;
 const MAX_FAILURE_RATE: f64 = 0.7;
 
+/// Calculates the daily rewards reduction based on the failure rate.
+///
+/// # Arguments
+///
+/// * `failure_rate` - A reference to a `f64` value representing the failure rate for the day.
+///
+/// # Returns
+///
+/// * A `f64` value representing the rewards reduction for the day, where:
+///   - `0.0` indicates no reduction (failure rate below the minimum threshold),
+///   - `1.0` indicates maximum reduction (failure rate above the maximum threshold),
+///   - A value between `0.0` and `1.0` represents a proportional reduction based on the failure rate.
+///
+/// # Explanation
+///
+/// 1. The function checks if the provided `failure_rate` is below the `MIN_FAILURE_RATE` -> no reduction in rewards.
+///
+/// 2. It then checks if the `failure_rate` is above the `MAX_FAILURE_RATE` -> maximum reduction in rewards.
+///
+/// 3. If the `failure_rate` is within the defined range (`MIN_FAILURE_RATE` to `MAX_FAILURE_RATE`),
+///    the function calculates the reduction proportionally:
+///    - The reduction is calculated by normalizing the `failure_rate` within the range, resulting in a value between `0.0` and `1.0`.
 fn daily_rewards_reduction(failure_rate: &f64) -> f64 {
     if failure_rate < &MIN_FAILURE_RATE {
         0.0
@@ -37,9 +59,30 @@ impl DailyNodeMetrics {
     }
 }
 
+/// Computes the rewards percentage based on the daily reward reductions WITH consecutive days penalty.
+///
+/// # Arguments
+///
+/// * `daily_metrics` - A slice of `DailyNodeMetrics` structs, where each struct represents the metrics for a single day.
+///
+/// # Returns
+///
+/// * A `f64` value representing the rewards percentage after reductions and penalties, rounded to two decimal places.
+///
+/// # Explanation
+///
+/// 1. The function calculates the number of active days.
+///
+/// 2. The function iterates over each day's metrics:
+///    - For each day with a non-zero reduction, it adds the reduction to the cumulative penalty and increments the consecutive day penalty counter.
+///    - For each day with zero reduction (`daily_reduction == 0.0`), if there was a streak of consecutive days with reductions,
+///      it adds the cumulative penalty to the `reduction_sum` and resets the streak counters.
+///
+/// 3. The overall reduction is calculated by dividing the total reduction (`reduction_sum`) by the number of active days.
+///    The reduction is then normalized by ensuring it does not exceed 1.0.
 pub fn rewards_with_penalty(daily_metrics: &[DailyNodeMetrics]) -> f64 {
     let active_days = daily_metrics.len();
-    let mut reduction = 0.0;
+    let mut reduction_sum = 0.0;
     let mut consecutive_reduction = 0.0;
     let mut consecutive_count = 0;
 
@@ -47,42 +90,56 @@ pub fn rewards_with_penalty(daily_metrics: &[DailyNodeMetrics]) -> f64 {
         // Just if we want to count the day unassigned as 0.0 reduction
         // we would need to check if previous daily metrics is <= 24hrs
         // before current metrics
-        let daily_rewards: f64 = metrics.rewards_reduction;
+        let daily_reduction: f64 = metrics.rewards_reduction;
 
-        if daily_rewards == 0.0 {
+        if daily_reduction == 0.0 {
             if consecutive_count > 0 {
-                reduction += consecutive_reduction * consecutive_count as f64;
+                reduction_sum += consecutive_reduction * consecutive_count as f64;
                 consecutive_reduction = 0.0;
                 consecutive_count = 0;
             }
         } else {
-            consecutive_reduction += daily_rewards;
+            consecutive_reduction += daily_reduction;
             consecutive_count += 1;
         }
     }
 
     // Handles the last consecutive days
     if consecutive_count > 0 {
-        reduction += consecutive_reduction * consecutive_count as f64;
+        reduction_sum += consecutive_reduction * consecutive_count as f64;
     }
 
-    reduction /= active_days as f64;
-    let reduction_normalized = reduction.min(1.0);
+    let overall_reduction = reduction_sum / active_days as f64;
+    let reduction_normalized = overall_reduction.min(1.0);
 
     ((1.0 - reduction_normalized) * 100.0).round() / 100.0
 }
 
+/// Rewards no penalty
+///
+/// Computes the rewards percentage based on the daily reward reductions WITHOUT consecutive days penalty.
+/// `overall_reduction` is defined as the average of the daily reward reductions.
+///
+/// # Arguments
+///
+/// * `daily_metrics` - A slice of `DailyNodeMetrics` structs, where each struct represents the metrics for a single day.
+///
+/// # Returns
+///
+/// * A `f64` value representing the rewards percentage after overall reductions, rounded to two decimal places.
+///
+/// # Explanation
+///
+/// 1. The function iterates through each day's metrics, summing up the `rewards_reduction` values across all days.
+/// 2. The `overall_reduction` is calculated by dividing the total `reduction_sum` by the number of active days.
+/// 3. Finally, the function calculates the rewards percentage without penalty by subtracting `overall_reduction` from 1.0,
+///    multiplying by 100, and rounding to two decimal places before returning the result.
 pub fn rewards_no_penalty(daily_metrics: &[DailyNodeMetrics]) -> f64 {
     let active_days = daily_metrics.len();
+    let reduction_sum: f64 = daily_metrics.iter().map(|metrics| metrics.rewards_reduction).sum();
+    let overall_reduction = reduction_sum / (active_days as f64);
 
-    let reduction = daily_metrics.iter().fold(0.0, |mut acc, metrics| {
-        let daily_rewards = metrics.rewards_reduction;
-
-        acc += daily_rewards;
-        acc
-    }) / (active_days as f64);
-
-    ((1.0 - reduction) * 100.0).round() / 100.0
+    ((1.0 - overall_reduction) * 100.0).round() / 100.0
 }
 
 #[cfg(test)]
