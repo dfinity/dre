09002eec36bae121c566a0d2d905018f718dbadd
Saša Tomić <sasa.tomic@dfinity.org>
2024-09-06 10:17:30 +0200

    chore(ci): Use the same cargo-deny-checks.sh script locally and on the CI

--- bin/cargo-deny-checks.sh
+++ bin/cargo-deny-checks.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+command -v cargo >/dev/null || {
+    if test -x "$HOME/.cargo/bin/cargo"; then
+        export PATH="$HOME/.cargo/bin:$PATH"
+    else
+        echo "'cargo' not found. Please install it by following the instructions at https://doc.rust-lang.org/cargo/getting-started/installation.html"
+        exit 1
+    fi
+}
+
 command -v cargo-deny >/dev/null || echo "'cargo-deny' not found. Please install it by running 'cargo install cargo-deny'"
 cargo deny check
