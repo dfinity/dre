9d5f521c8faebed4433612a981dbddfc980efceb
Saša Tomić <sasa.tomic@dfinity.org>
2024-09-06 10:01:43 +0200

    exclude one advisory only

--- bin/cargo-deny-checks.sh
+++ bin/cargo-deny-checks.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 command -v cargo-deny >/dev/null || echo "'cargo-deny' not found. Please install it by running 'cargo install cargo-deny'"
-cargo deny check --warn unmaintained
+cargo deny check
