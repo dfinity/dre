d4ec264695e951ce6695713babd7fc51a79746ed
Luka Skugor <luka.skugor@chimeramail.com>
2024-09-11 10:55:33 +0200

    Update release-controller/reconciler.py

--- release-controller/reconciler.py
+++ release-controller/reconciler.py
@@ -67,7 +67,7 @@ class ReconcilerState:
                 )
             else:
                 last_modified = datetime.datetime.fromtimestamp(os.path.getmtime(version_path))
-                remaining_time_until_retry = datetime.timedelta(minutes=3) - (
+                remaining_time_until_retry = datetime.timedelta(minutes=10) - (
                     datetime.datetime.now() - last_modified
                 )
                 if remaining_time_until_retry.total_seconds() > 0:
