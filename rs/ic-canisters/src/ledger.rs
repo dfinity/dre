596c6abf4b53659cdc32cb5f1a841748bbe1c272
nikolamilosa <nikolamilosa20@gmail.com>
2024-11-04 14:13:56 +0100

    fixing fetching of correct account

---
+++ rs/ic-canisters/src/ledger.rs
@@ -0,0 +1,26 @@
+use ic_nns_constants::{GOVERNANCE_CANISTER_ID, LEDGER_CANISTER_ID};
+use icrc_ledger_types::icrc1::account::Account;
+
+use crate::IcAgentCanisterClient;
+
+pub struct LedgerCanisterWrapper {
+    agent: IcAgentCanisterClient,
+}
+
+impl From<IcAgentCanisterClient> for LedgerCanisterWrapper {
+    fn from(value: IcAgentCanisterClient) -> Self {
+        Self { agent: value }
+    }
+}
+
+impl LedgerCanisterWrapper {
+    pub async fn get_account_id(&self, subaccount: Option<[u8; 32]>) -> anyhow::Result<Vec<u8>> {
+        let args = Account {
+            owner: GOVERNANCE_CANISTER_ID.into(),
+            subaccount,
+        };
+        self.agent
+            .query::<Vec<u8>>(&LEDGER_CANISTER_ID.into(), "account_identifier", candid::encode_one(args)?)
+            .await
+    }
+}
