e0189474c9e5e93547bab5d7a2049c62ebdc86d0
Nikola Milosavljevic <73236646+NikolaMilosa@users.noreply.github.com>
2024-11-12 11:33:23 +0100

    feat: ensure that there are some publishable changes in release notes (#1068)

--- release-controller/publish_notes.py
+++ release-controller/publish_notes.py
@@ -8,6 +8,8 @@ from github import Github
 from github.Repository import Repository
 import markdown
 from itertools import groupby
+from google_docs import ReleaseNotesClient
+import pathlib
 
 REPLICA_RELEASES_DIR = "replica-releases"
 
@@ -103,14 +105,14 @@ class PublishNotesClient:
     def publish_if_ready(self, google_doc_markdownified, version: str):
         """Publish the release notes if they are ready."""
         if not isinstance(google_doc_markdownified, str):
-            logging.info("didn't get markdown notes for %s, skipping", version)
+            logging.warning("didn't get markdown notes for %s, skipping", version)
             return
 
         changelog = post_process_release_notes(google_doc_markdownified)
 
         release_notes_start = changelog.find("Release Notes")
         if release_notes_start == -1:
-            logging.warning("could not find release notes section for version %s", version)
+            logging.error("could not find release notes section for version %s", version)
             return
 
         if not re.match(
@@ -121,15 +123,53 @@ class PublishNotesClient:
             return
 
         changelog = changelog[release_notes_start:]
+        if check_number_of_changes(changelog) == 0:
+            logging.error("release notes for version %s contain no commits that would be published.")
+            return
         # TODO: parse markdown to check formatting is correct
         self.ensure_published(version=version, changelog=changelog)
 
+def check_number_of_changes(changelog: str) -> int:
+    BEGINNING_MARKER = "To see a full list of commits added since last release"
+    ENDING_MARKER = "## Excluded Changes"
+
+    num_changes = 0
+    found_beginning = False
+    for line in changelog.splitlines():
+        print("Processing line whole:", line)
+        if not found_beginning and line.startswith(BEGINNING_MARKER):
+            found_beginning = True
+            continue
+
+        if found_beginning:
+            print("Processing line:", line)
+            if line.startswith(ENDING_MARKER):
+                break
+            if line.startswith("*"):
+                num_changes += 1
+
+    return num_changes
 
 def main():
     load_dotenv()
     github_client = Github(auth=Auth.Token(os.environ["GITHUB_TOKEN"]))
     client = PublishNotesClient(github_client.get_repo("dfinity/dre-testing"))
     client.ensure_published("85bd56a70e55b2cea75cae6405ae11243e5fdad8", "test")
+
+    # For testing the `check_number_of_changes`
+    release_notes_client = ReleaseNotesClient(
+        credentials_file=pathlib.Path(
+            os.environ.get(
+                "GDOCS_CREDENTIALS_PATH",
+                pathlib.Path(__file__).parent.resolve() / "credentials.json",
+            )
+        )
+    )
+    # Would not publish this one
+    version = "c6847128f3a872e0e084b2920bfcd21f881c69fa"
+    # Should publish this one
+    # version = "f88938214b16584075196e13d0af7c50f671131a"
+    client.publish_if_ready(release_notes_client.markdown_file(version), version)
 
 
 if __name__ == "__main__":
