70fde31a11d63d942ccd5d9be9ac95624a7e0638
Saša Tomić <sasa.tomic@dfinity.org>
2024-11-13 10:24:42 +0100

    feat(dre,network): add "network --ensure-operator-nodes-unassigned"
    This should give us a chance to further improve network topology and decentralization.

--- rs/cli/src/commands/network.rs
+++ rs/cli/src/commands/network.rs
@@ -1,4 +1,5 @@
 use clap::Args;
+use log::info;
 
 use super::{AuthRequirement, ExecutableCommand};
 
@@ -14,6 +15,12 @@ pub struct Network {
     /// this does not worsen the decentralization of the target subnet.
     #[clap(long)]
     pub ensure_operator_nodes_assigned: bool,
+
+    /// Ensure that at least one node of each node operator is
+    /// not assigned to any subnet. Node will only be unassigned from a subnet if
+    /// this does not worsen the decentralization of the target subnet.
+    #[clap(long)]
+    pub ensure_operator_nodes_unassigned: bool,
 
     /// Skip provided subnets.
     #[clap(long, num_args(1..))]
@@ -31,6 +38,7 @@ impl ExecutableCommand for Network {
         let mut errors = vec![];
         let network_heal = self.heal || std::env::args().any(|arg| arg == "heal");
         if network_heal {
+            info!("Healing the network by replacing unhealthy nodes and optimizing decentralization in subnets that have unhealthy nodes");
             let proposals = runner.network_heal(ctx.forum_post_link(), &self.skip_subnets).await?;
             for proposal in proposals {
                 if let Err(e) = ic_admin.propose_run(proposal.cmd, proposal.opts).await {
@@ -38,9 +46,10 @@ impl ExecutableCommand for Network {
                 }
             }
         } else {
-            log::info!("No network healing requested: ");
+            info!("No network healing requested");
         }
         if self.ensure_operator_nodes_assigned {
+            info!("Ensuring some operator nodes are assigned, for every node operator");
             let proposals = runner
                 .network_ensure_operator_nodes_assigned(ctx.forum_post_link(), &self.skip_subnets)
                 .await?;
@@ -49,6 +58,21 @@ impl ExecutableCommand for Network {
                     errors.push(e);
                 }
             }
+        } else {
+            info!("No network ensure operator nodes assigned requested");
+        }
+        if self.ensure_operator_nodes_unassigned {
+            info!("Ensuring some operator nodes are unassigned, for every node operator");
+            let proposals = runner
+                .network_ensure_operator_nodes_unassigned(ctx.forum_post_link(), &self.skip_subnets)
+                .await?;
+            for proposal in proposals {
+                if let Err(e) = ic_admin.propose_run(proposal.cmd, proposal.opts).await {
+                    errors.push(e);
+                }
+            }
+        } else {
+            info!("No network ensure operator nodes unassigned requested");
         }
         match errors.is_empty() {
             true => Ok(()),
@@ -59,10 +83,10 @@ impl ExecutableCommand for Network {
     fn validate(&self, _args: &crate::commands::Args, cmd: &mut clap::Command) {
         // At least one of the two options must be provided
         let network_heal = self.heal || std::env::args().any(|arg| arg == "heal");
-        if !network_heal && !self.ensure_operator_nodes_assigned {
+        if !network_heal && !self.ensure_operator_nodes_assigned && !self.ensure_operator_nodes_unassigned {
             cmd.error(
                 clap::error::ErrorKind::MissingRequiredArgument,
-                "At least one of '--heal' or '--ensure-operator-nodes-assigned' must be specified.",
+                "At least one of '--heal' or '--ensure-operator-nodes-assigned' or '--ensure-operator-nodes-unassigned' must be specified.",
             )
             .exit()
         }
