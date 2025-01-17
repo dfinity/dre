32313b1d24135029f51ee06939be6bd6e32dba54
Saša Tomić <sasa.tomic@dfinity.org>
2025-01-17 20:03:32 +0100

    refactor(cli): simplify neuron balance command
    ### Removed
    - Eliminate `neuron_override` option from `Balance` struct.
    
    ### Updated
    - Simplify `require_auth` method to always require `AuthRequirement::Neuron`.
    - Use `ctx.neuron_id()` to determine neuron ID, providing a fallback if none is specified.
    
    ### Added
    - Introduce `neuron_id` method in `DreContext` to fetch neuron ID option.
    
    ### Note
    - Updated related tests and comments to reflect these changes.

--- rs/cli/src/commands/neuron/balance.rs
+++ rs/cli/src/commands/neuron/balance.rs
@@ -4,18 +4,11 @@ use ic_canisters::governance::GovernanceCanisterWrapper;
 use crate::commands::ExecutableCommand;
 
 #[derive(Args, Debug)]
-pub struct Balance {
-    /// Neuron to query, by default will use the one from configured identity
-    #[clap(long)]
-    neuron_override: Option<u64>,
-}
+pub struct Balance {}
 
 impl ExecutableCommand for Balance {
     fn require_auth(&self) -> crate::commands::AuthRequirement {
-        match &self.neuron_override {
-            Some(_) => crate::commands::AuthRequirement::Anonymous,
-            None => crate::commands::AuthRequirement::Neuron,
-        }
+        crate::commands::AuthRequirement::Neuron
     }
 
     fn validate(&self, _args: &crate::commands::Args, _cmd: &mut clap::Command) {}
@@ -23,10 +16,7 @@ impl ExecutableCommand for Balance {
     async fn execute(&self, ctx: crate::ctx::DreContext) -> anyhow::Result<()> {
         let (neuron, client) = ctx.create_ic_agent_canister_client().await?;
         let governance = GovernanceCanisterWrapper::from(client);
-        let neuron_id = match self.neuron_override {
-            Some(n) => n,
-            None => neuron.neuron_id,
-        };
+        let neuron_id = ctx.neuron_id().unwrap_or(neuron.neuron_id);
         let neuron_info = governance.get_neuron_info(neuron_id).await?;
 
         println!("{}", neuron_info.stake_e8s / 10_u64.pow(8));
