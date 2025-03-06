6da81a6bb29a5e247280982ab4fec12a59bcf643
nikolamilosa <nikola.milosavljevic@dfinity.org>
2025-03-06 13:31:27 +0100

    fmt

--- rs/ic-observability/general-testnet-service-discovery/src/server/get_targets.rs
+++ rs/ic-observability/general-testnet-service-discovery/src/server/get_targets.rs
@@ -6,9 +6,5 @@ use crate::{storage::Storage, supervisor::TargetSupervisor};
 use super::WebResult;
 
 pub(super) async fn get_targets(State(supervisor): State<TargetSupervisor>) -> WebResult<Json<Vec<JournaldTarget>>> {
-    supervisor
-        .get()
-        .await
-        .map_err(|e| (StatusCode::BAD_REQUEST, e.to_string()))
-        .map(Json)
+    supervisor.get().await.map_err(|e| (StatusCode::BAD_REQUEST, e.to_string())).map(Json)
 }
