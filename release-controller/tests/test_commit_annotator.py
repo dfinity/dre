f72207c087c7caea06b8fa5352421a311c3f8e55
Manuel Amador <manuel.amador@dfinity.org>
2025-03-13 16:51:55 +0100

    Clean up long tests (minimizing diff to upstream) to ensure that the tests are faster -- cache the IC repo to be used downstream in the long tests, while keeping the parallelism of the original tests.

--- release-controller/tests/test_commit_annotator.py
+++ release-controller/tests/test_commit_annotator.py
@@ -1,34 +1,44 @@
 import logging
+import os
 import pathlib
+import pytest
+import tarfile
 import tempfile
+import typing
 
 from commit_annotator import target_determinator
-from git_repo import GitRepo
+from git_repo import GitRepo, GitRepoAnnotator
 
 
 _LOGGER = logging.getLogger()
 
 
-def test_determinator_touchstones() -> None:
-    table = [
-        ("not a guestos change", "00dc67f8d", False),
-        ("bumped dependencies", "2d0835bba", True),
-        ("github dir changed", "94fd38099", False),
-        ("replica changed", "951e895c7", True),
-        ("cargo lock paths only", "5a250cb34", False),
-    ]
-    # It would be fantastic if this clone could be a @pytest.fixture
-    # but, due to our use of the xdist pytest plugin, that provides
-    # no savings (each process clones its own repo, rather than reusing
-    # the fixtures throughout the session).  It would still not be useful
-    # to parallelize the different tests on the table above, since the
-    # GitRepo class is not thread-safe anyway.
+# It would be fantastic if this clone could be a plain @pytest.fixture
+# but, due to our use of the xdist pytest plugin, that provides
+# no savings (each process clones its own IC repo, rather than reusing
+# the fixtures throughout the session).  It would still not be useful
+# to parallelize the different tests on the table above, since the
+# GitRepo class is not thread-safe anyway.
+# So what do we do here?  We look for a variable (present in Bazel
+# tests) that signals a promise that the tarball named in the variable
+# contains an up-to-date clone of the IC repo.  If we find it, we
+# extract it prior to instantiating the IC repo clone.
+@pytest.fixture(scope="session")
+def annotator() -> typing.Generator[GitRepoAnnotator, None, None]:
     with tempfile.TemporaryDirectory() as d:
-        _LOGGER.info("Cloning IC repo...")
+        cache_dir = pathlib.Path(d)
+        if "IC_REPO_SEED_TAR" in os.environ:
+            unpacked_dir = cache_dir / "github.com" / "dfinity" / "ic.git"
+            _LOGGER.info("Unpacking seed tarball of IC repo to %s...", unpacked_dir)
+            os.makedirs(unpacked_dir, exist_ok=True)
+            with tarfile.open(os.environ["IC_REPO_SEED_TAR"]) as seed_repo:
+                seed_repo.extractall(unpacked_dir, filter="tar")
+        else:
+            _LOGGER.info("Cloning IC repo under cache directory %s...", cache_dir)
         ic_repo = GitRepo(
             "https://github.com/dfinity/ic.git",
             main_branch="master",
-            repo_cache_dir=pathlib.Path(d),
+            repo_cache_dir=cache_dir,
             behavior={
                 "push_annotations": False,
                 "save_annotations": True,
@@ -37,9 +47,30 @@ def test_determinator_touchstones() -> None:
         )
         _LOGGER.info("Clone of IC repo finished.  Going into annotator context.")
         with ic_repo.annotator([]) as annotator:
-            for explanation, object, expected_result in table:
-                _LOGGER.info(f"Testing touchstone {explanation}...")
-                actual_result = target_determinator(object=object, ic_repo=annotator)
-                assert (
-                    actual_result == expected_result
-                ), f"While running touchstone {explanation} on commit {object}, result {actual_result} != expected {expected_result}"
+            yield annotator
+
+
+def _test_guestos_changed(
+    ann: GitRepoAnnotator, object: str, expect_changed: bool
+) -> None:
+    assert target_determinator(object=object, ic_repo=ann) == expect_changed
+
+
+def test_guestos_changed__not_guestos_change(annotator: GitRepoAnnotator) -> None:
+    _test_guestos_changed(annotator, object="00dc67f8d", expect_changed=False)
+
+
+def test_guestos_changed__bumped_dependencies(annotator: GitRepoAnnotator) -> None:
+    _test_guestos_changed(annotator, object="2d0835bba", expect_changed=True)
+
+
+def test_guestos_changed__github_dir_changed(annotator: GitRepoAnnotator) -> None:
+    _test_guestos_changed(annotator, object="94fd38099", expect_changed=False)
+
+
+def test_guestos_changed__replica_changed(annotator: GitRepoAnnotator) -> None:
+    _test_guestos_changed(annotator, object="951e895c7", expect_changed=True)
+
+
+def test_guestos_changed__cargo_lock_paths_only(annotator: GitRepoAnnotator) -> None:
+    _test_guestos_changed(annotator, object="5a250cb34", expect_changed=False)
