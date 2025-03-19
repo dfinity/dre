bab9d0761513f4f3e64a27ceb96a9717fe942ab7
Manuel Amador <manuel.amador@dfinity.org>
2025-03-19 14:12:50 +0100

    feat: Add Prometheus telemetry to node provider rewards canister.
    This commit adds very basic support for Prometheus telemetry (including a mechanism to encode metrics using the `ic-metrics-encoder` crate).
    
    The metrics added are:
    
    * Generic memory usage coming from IC NNS metrics code.
    * Metrics calculation start, success and end timestamps.

--- rs/dre-canisters/node-provider-rewards/src/lib.rs
+++ rs/dre-canisters/node-provider-rewards/src/lib.rs
@@ -1,8 +1,12 @@
+use std::cell::RefCell;
+
 use crate::canister_client::ICCanisterClient;
 use crate::metrics::MetricsManager;
 use crate::registry_store::CanisterRegistryStore;
 use crate::storage::{State, VM};
+use ic_canisters_http_types::{HttpRequest, HttpResponse, HttpResponseBuilder};
 use ic_cdk_macros::*;
+use ic_nervous_system_common::serve_metrics;
 
 mod canister_client;
 mod metrics;
@@ -19,11 +23,42 @@ pub type RegistryStoreInstance = CanisterRegistryStore<State, VM>;
 pub type MetricsManagerInstance = MetricsManager<State, VM>;
 pub const IC_CANISTER_CLIENT: ICCanisterClient = ICCanisterClient {};
 
+#[derive(Default)]
+pub struct PrometheusMetrics {
+    last_calculation_start: f64,
+    last_calculation_success: f64,
+    last_calculation_end: f64,
+}
+
+impl PrometheusMetrics {
+    fn new() -> Self {
+        Default::default()
+    }
+
+    fn mark_last_calculation_start(&mut self) {
+        self.last_calculation_start = (ic_cdk::api::time() / 1_000_000_000) as f64
+    }
+
+    fn mark_last_calculation_success(&mut self) {
+        self.last_calculation_end = (ic_cdk::api::time() / 1_000_000_000) as f64;
+        self.last_calculation_success = self.last_calculation_end
+    }
+
+    fn mark_last_calculation_end(&mut self) {
+        self.last_calculation_end = (ic_cdk::api::time() / 1_000_000_000) as f64
+    }
+}
+
+thread_local! {
+    pub(crate) static PROMETHEUS_METRICS: RefCell<PrometheusMetrics> = RefCell::new(PrometheusMetrics::new());
+}
+
 /// Sync the local registry and subnets metrics with remote
 ///
 /// - Sync local registry stored from the remote registry canister
 /// - Sync subnets metrics from the management canister of the different subnets
 async fn sync_all() {
+    PROMETHEUS_METRICS.with_borrow_mut(|m| m.mark_last_calculation_start());
     let registry_sync_result = RegistryStoreInstance::sync_registry_stored(&IC_CANISTER_CLIENT).await;
 
     match registry_sync_result {
@@ -31,9 +66,13 @@ async fn sync_all() {
             let subnets_list = registry::subnets_list();
             MetricsManagerInstance::update_subnets_metrics(&IC_CANISTER_CLIENT, subnets_list).await;
 
+            PROMETHEUS_METRICS.with_borrow_mut(|m| m.mark_last_calculation_success());
             ic_cdk::println!("Successfully synced subnets metrics and local registry");
         }
-        Err(e) => ic_cdk::println!("Failed to sync local registry: {:?}", e),
+        Err(e) => {
+            PROMETHEUS_METRICS.with_borrow_mut(|m| m.mark_last_calculation_end());
+            ic_cdk::println!("Failed to sync local registry: {:?}", e)
+        }
     }
 }
 
@@ -61,4 +100,62 @@ fn init() {
 #[post_upgrade]
 fn post_upgrade() {
     setup_timers();
+}
+
+pub fn prometheus_metrics() -> &'static PrometheusMetrics {
+    unsafe { &*PROMETHEUS_METRICS.with(|g| g.as_ptr()) }
+}
+
+pub fn encode_metrics(metrics: &PrometheusMetrics, w: &mut ic_metrics_encoder::MetricsEncoder<Vec<u8>>) -> std::io::Result<()> {
+    // General resource consumption.
+    w.encode_gauge(
+        "canister_stable_memory_size_bytes",
+        ic_nervous_system_common::stable_memory_size_bytes() as f64,
+        "Size of the stable memory allocated by this canister measured in bytes.",
+    )?;
+    w.encode_gauge(
+        "canister_total_memory_size_bytes",
+        ic_nervous_system_common::total_memory_size_bytes() as f64,
+        "Size of the total memory allocated by this canister measured in bytes.",
+    )?;
+
+    // Calculation signals.
+
+    // Calculation start timestamp seconds.
+    //
+    // * 0.0 -> first calculation not yet begun since canister started.
+    // * Any other positive number -> at least one calculation has started.
+    w.encode_gauge(
+        "last_calculation_start_timestamp_seconds",
+        metrics.last_calculation_start,
+        "Last time the calculation of metrics started.  If this metric is present but zero, the first calculation during this canister's current execution has not yet begun or taken place.",
+    )?;
+    // Calculation finish timestamp seconds.
+    // * 0.0 -> first calculation not yet finished since canister started.
+    // * last_calculation_end_timestamp_seconds - last_calculation_start_timestamp_seconds > 0 -> last calculation finished, next calculation not started yet
+    // * last_calculation_end_timestamp_seconds - last_calculation_start_timestamp_seconds < 0 -> calculation ongoing, not finished yet
+    w.encode_gauge(
+        "last_calculation_end_timestamp_seconds",
+        metrics.last_calculation_end,
+        "Last time the calculation of metrics ended (successfully or with failure).  If this metric is present but zero, the first calculation during this canister's current execution has not started or finished yet, either successfully or with errors.   Else, subtracting this from the last calculation start should yield a positive value if the calculation ended (successfully or with errors), and a negative value if the calculation is still ongoing but has not finished.",
+    )?;
+    // Calculation success timestamp seconds.
+    // * 0.0 -> no calculation has yet succeeded since canister started.
+    // * last_calculation_end_timestamp_seconds == last_calculation_success_timestamp_seconds -> last calculation finished successfully
+    // * last_calculation_end_timestamp_seconds != last_calculation_success_timestamp_seconds -> last calculation failed
+    w.encode_gauge(
+        "last_calculation_success_timestamp_seconds",
+        metrics.last_calculation_success,
+        "Last time the calculation of metrics succeeded.  If this metric is present but zero, no calculation has yet succeeded during this canister's current execution.  Else, subtracting this number from last_calculation_start_timestamp_seconds gives a positive time delta when the last calculation succeeded, or a negative value if either the last calculation failed or a calculation is currently being performed.  By definition, this and last_calculation_end_timestamp_seconds will be identical when the last calculation succeeded.",
+    )?;
+
+    Ok(())
+}
+
+#[query(hidden = true, decoding_quota = 10000)]
+fn http_request(request: HttpRequest) -> HttpResponse {
+    match request.path() {
+        "/metrics" => serve_metrics(|encoder| encode_metrics(prometheus_metrics(), encoder)),
+        _ => HttpResponseBuilder::not_found().build(),
+    }
 }
