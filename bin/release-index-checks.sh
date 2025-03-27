4a3fb58740a3cc6fd1de62ca2eb44faea0bbfbc6
Manuel Amador <manuel.amador@dfinity.org>
2025-03-27 13:59:29 +0100

    Improvements to release index checks.
    * Leverage Github summaries.
    * Move the CI check file **outside** of release-controller, where it does not belong because it's not part of the release controller.

--- bin/release-index-checks.sh
+++ bin/release-index-checks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-files=("release-index-schema.json" "release-index.yaml" "release-controller/ci_check.py")
+files=("release-index-schema.json" "release-index.yaml" "release_index_ci_check.py" "release-controller/git_repo.py")
 
 found=false
 
@@ -15,4 +15,5 @@ if [[ "$found" == false ]]; then
     exit 0
 fi
 command -v rye >/dev/null || echo "'rye' not found. Please install it by following the instructions from https://rye.astral.sh/"
-rye run python release-controller/ci_check.py --repo-path ~/.cache/git/ic
+export PYTHONPATH=$PWD/release-controller
+rye run python release_index_ci_check.py --repo-path ~/.cache/git/ic
