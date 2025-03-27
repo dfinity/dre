be10996be7591a77eee1b4847db817eaaf0d25c5
Manuel Amador <manuel.amador@dfinity.org>
2025-03-27 14:02:21 +0100

    Move script to final destination.

--- bin/release-index-checks.sh
+++ bin/release-index-checks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-files=("release-index-schema.json" "release-index.yaml" "release_index_ci_check.py" "release-controller/git_repo.py")
+files=("release-index-schema.json" "release-index.yaml" "scripts/release_index_ci_check.py" "release-controller/git_repo.py")
 
 found=false
 
@@ -16,4 +16,4 @@ if [[ "$found" == false ]]; then
 fi
 command -v rye >/dev/null || echo "'rye' not found. Please install it by following the instructions from https://rye.astral.sh/"
 export PYTHONPATH=$PWD/release-controller
-rye run python release_index_ci_check.py --repo-path ~/.cache/git/ic
+rye run python scripts/release_index_ci_check.py --repo-path ~/.cache/git/ic
