ddad7c9ecbc258f0565153263144fef0eb3f951d
DFINITYManu <123385598+DFINITYManu@users.noreply.github.com>
2025-04-17 14:34:50 +0200

    Instruction counter metrics. (#1480)

--- rs/dre-canisters/node-provider-rewards/canister/src/lib.rs
+++ rs/dre-canisters/node-provider-rewards/canister/src/lib.rs
@@ -23,15 +23,26 @@ const DAY_IN_SECONDS: u64 = HOUR_IN_SECONDS * 24;
 /// - Sync local registry stored from the remote registry canister
 /// - Sync subnets metrics from the management canister of the different subnets
 async fn sync_all() {
+    let mut instruction_counter = telemetry::InstructionCounter::new();
     telemetry::PROMETHEUS_METRICS.with_borrow_mut(|m| m.mark_last_sync_start());
     let registry_store = REGISTRY_STORE.with(|m| m.clone());
 
-    match registry_store.schedule_registry_sync().await {
+    instruction_counter.lap();
+    let result = registry_store.schedule_registry_sync().await;
+    let registry_sync_instructions = instruction_counter.lap();
+
+    let mut subnet_list_instructions: u64 = 0;
+    let mut update_subnet_metrics_instructions: u64 = 0;
+
+    match result {
         Ok(_) => {
             let metrics_manager = METRICS_MANAGER.with(|m| m.clone());
+            instruction_counter.lap(); // Reset the lap time.
             let subnets_list = registry_store.subnets_list();
-
+            subnet_list_instructions = instruction_counter.lap();
             metrics_manager.update_subnets_metrics(subnets_list).await;
+            update_subnet_metrics_instructions = instruction_counter.lap();
+
             telemetry::PROMETHEUS_METRICS.with_borrow_mut(|m| m.mark_last_sync_success());
             ic_cdk::println!("Successfully synced subnets metrics and local registry");
         }
@@ -40,6 +51,15 @@ async fn sync_all() {
             ic_cdk::println!("Failed to sync local registry: {:?}", e)
         }
     }
+
+    telemetry::PROMETHEUS_METRICS.with_borrow_mut(|m| {
+        m.record_last_sync_instructions(
+            instruction_counter.sum(),
+            registry_sync_instructions,
+            subnet_list_instructions,
+            update_subnet_metrics_instructions,
+        )
+    });
 }
 
 fn setup_timers() {
@@ -74,7 +94,7 @@ fn post_upgrade() {
 #[query(hidden = true, decoding_quota = 10000)]
 fn http_request(request: HttpRequest) -> HttpResponse {
     match request.path() {
-        "/metrics" => serve_metrics(|encoder| telemetry::PROMETHEUS_METRICS.with(|m| telemetry::encode_metrics(&m.borrow(), encoder))),
+        "/metrics" => serve_metrics(|encoder| telemetry::PROMETHEUS_METRICS.with(|m| m.borrow().encode_metrics(encoder))),
         _ => HttpResponseBuilder::not_found().build(),
     }
 }
