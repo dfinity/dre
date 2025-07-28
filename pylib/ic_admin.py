b96cb1a60b746ab9744612e63d9d76680f0f6deb
NikolaMilosa <73236646+NikolaMilosa@users.noreply.github.com>
2025-07-28 02:24:53 +0000

    chore(deps): Update dependencies

--- pylib/ic_admin.py
+++ pylib/ic_admin.py
@@ -152,6 +152,6 @@ def canister_version(agent: Agent, canister_principal: str) -> str:
 
 if __name__ == "__main__":
     # One can run some simple one-off tests here, e.g.:
-    ic_admin = IcAdmin("https://ic0.app", git_revision="2f87fe95207dc6371a2f2dc273362ba03b41e0e9")
+    ic_admin = IcAdmin("https://ic0.app", git_revision="f8f04575d05560f1ca7cbfa988d6f107d05b1822")
 
     print(ic_admin.get_subnet_replica_versions())
