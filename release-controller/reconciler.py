143d41648e42b15426991f0fe61d3874ed3f7a39
Manuel Amador <manuel.amador@dfinity.org>
2025-08-21 12:56:09 +0200

    Fix parameter name.

--- release-controller/reconciler.py
+++ release-controller/reconciler.py
@@ -1021,7 +1021,7 @@ def oneoff() -> None:
         unelect_versions=[],
         package_checksum=version_package_checksum(version, GUESTOS),
         package_urls=version_package_urls(version, GUESTOS),
-        launch_measurements_path=None,
+        launch_measurements=None,
     )
 
 
